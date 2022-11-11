e52803136d8246b3ec0447098ba5f562a751c417
lipeeeee <filipe.pereirafga@gmail.com>
2022-11-11 19:59:22 +0000

    Neural Network done

--- main.py
+++ main.py
@@ -4,73 +4,68 @@ from matplotlib import pyplot as plt
 
 # Get data and store in np array
 data = pd.read_csv("./digit-recognizer/train.csv")
-m, n = data.shape
-data = np.array(data)
-np.random.shuffle(data)
 
-# Divide in 2 datasets 
+data = np.array(data)
+m, n = data.shape
+np.random.shuffle(data) # shuffle before splitting into dev and training sets
+
 data_dev = data[0:1000].T
 Y_dev = data_dev[0]
 X_dev = data_dev[1:n]
+X_dev = X_dev / 255.
 
 data_train = data[1000:m].T
-Y_train = data_dev[0]
-X_train = data_dev[1:n]
+Y_train = data_train[0]
+X_train = data_train[1:n]
+X_train = X_train / 255.
+_,m_train = X_train.shape
 
 # Creating neural network
 def init_params():
-    W1 = np.random.rand(10, 784)
-    b1 = np.random.rand(10, 1)
-
-    W2 = np.random.rand(10, 10)
-    b2 = np.random.rand(10, 1)
-
+    W1 = np.random.rand(10, 784) - 0.5
+    b1 = np.random.rand(10, 1) - 0.5
+    W2 = np.random.rand(10, 10) - 0.5
+    b2 = np.random.rand(10, 1) - 0.5
     return W1, b1, W2, b2
 
 def ReLU(Z):
-    return np.maximum(0, Z)
+    return np.maximum(Z, 0)
 
 def softmax(Z):
-    return np.exp(Z) / np.sum(np.exp(Z))
-
-def foward_prop(W1, b1, W2, b2, X):
+    A = np.exp(Z) / sum(np.exp(Z))
+    return A
+    
+def forward_prop(W1, b1, W2, b2, X):
     Z1 = W1.dot(X) + b1
     A1 = ReLU(Z1)
-
     Z2 = W2.dot(A1) + b2
     A2 = softmax(Z2)
+    return Z1, A1, Z2, A2
 
-# one hot encoding
+def ReLU_deriv(Z):
+    return Z > 0
+
 def one_hot(Y):
-    # Creating empty matrix
-    # one_hot_Y = np.zeros((m, n + 1))
     one_hot_Y = np.zeros((Y.size, Y.max() + 1))
-    one_hot_Y[np.arrange(Y.size), Y] = 1
+    one_hot_Y[np.arange(Y.size), Y] = 1
     one_hot_Y = one_hot_Y.T
     return one_hot_Y
 
-def deriv_ReLU(Z):
-    return Z > 0
-
-def back_prop(Z1, A1, Z2, A2, W2, X, Y):
-    m = Y.size
-
+def backward_prop(Z1, A1, Z2, A2, W1, W2, X, Y):
     one_hot_Y = one_hot(Y)
     dZ2 = A2 - one_hot_Y
     dW2 = 1 / m * dZ2.dot(A1.T)
-    dB2 = 1 / m * np.sum(dZ2, 2)
-    dZ1 = W2.T.dot(dZ2) * deriv_ReLU(Z1)
+    db2 = 1 / m * np.sum(dZ2)
+    dZ1 = W2.T.dot(dZ2) * ReLU_deriv(Z1)
     dW1 = 1 / m * dZ1.dot(X.T)
-    dB1 = 1 / m * np.sum(dZ1, 2)
+    db1 = 1 / m * np.sum(dZ1)
+    return dW1, db1, dW2, db2
 
-    return dW1, dB1, dW2, dB2
-
-def update_params(W1, b1, W2, b2, dW1, dB1, dW2, dB2, alpha):
+def update_params(W1, b1, W2, b2, dW1, db1, dW2, db2, alpha):
     W1 = W1 - alpha * dW1
-    b1 = b1 - alpha * dB1
-    W2 = W2 - alpha * dW2
-    b2 = b2 - alpha * dB2
-
+    b1 = b1 - alpha * db1    
+    W2 = W2 - alpha * dW2  
+    b2 = b2 - alpha * db2    
     return W1, b1, W2, b2
 
 def get_predictions(A2):
@@ -80,15 +75,16 @@ def get_accuracy(predictions, Y):
     print(predictions, Y)
     return np.sum(predictions == Y) / Y.size
 
-def gradient_descent(X, Y, iterations, alpha):
+def gradient_descent(X, Y, alpha, iterations):
     W1, b1, W2, b2 = init_params()
-
     for i in range(iterations):
-        Z1, A1, Z2, A2 = foward_prop(W1, b1, W2, b2, X)
-        dW1, db1, dW2, db2 = back_prop(Z1, A1, Z2, A2, W2, X, Y)
+        Z1, A1, Z2, A2 = forward_prop(W1, b1, W2, b2, X)
+        dW1, db1, dW2, db2 = backward_prop(Z1, A1, Z2, A2, W1, W2, X, Y)
         W1, b1, W2, b2 = update_params(W1, b1, W2, b2, dW1, db1, dW2, db2, alpha)
-        
-        if i % 50 == 0:
+        if i % 10 == 0:
             print("Iteration: ", i)
-            print("Accuracy: ", get_accuracy(get_predictions(A2), Y))
+            predictions = get_predictions(A2)
+            print(get_accuracy(predictions, Y))
     return W1, b1, W2, b2
+
+W1, b1, W2, b2 = gradient_descent(X_train, Y_train, 0.10, 500)
