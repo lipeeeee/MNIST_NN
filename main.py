b23d83d38f4fc32c47a53bda54492397362679c0
lipeeeee <filipe.pereirafga@gmail.com>
2022-11-11 19:59:44 +0000

    Make prediction functionality

--- main.py
+++ main.py
@@ -87,4 +87,21 @@ def gradient_descent(X, Y, alpha, iterations):
             print(get_accuracy(predictions, Y))
     return W1, b1, W2, b2
 
+def make_predictions(X, W1, b1, W2, b2):
+    _, _, _, A2 = forward_prop(W1, b1, W2, b2, X)
+    predictions = get_predictions(A2)
+    return predictions
+
+def test_prediction(index, W1, b1, W2, b2):
+    current_image = X_train[:, index, None]
+    prediction = make_predictions(X_train[:, index, None], W1, b1, W2, b2)
+    label = Y_train[index]
+    print("Prediction: ", prediction)
+    print("Label: ", label)
+    
+    current_image = current_image.reshape((28, 28)) * 255
+    plt.gray()
+    plt.imshow(current_image, interpolation='nearest')
+    plt.show()
+
 W1, b1, W2, b2 = gradient_descent(X_train, Y_train, 0.10, 500)
